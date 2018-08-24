acb340407fefc71f5fa4e4fac6a6d3d524f5c7fc
Izan Calderaro <izancalderaro@yahoo.com.br>
2018-08-24 10:52:11 -0400

    Restful MongoDB

---
+++ servidorRESTful/todoListApi/server.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+ 
+  bodyParser = require('body-parser'),
+  servidor ='mongodb://localhost:27017/tododb',
+  load = require('express-load');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect(servidor,{ useNewUrlParser: true }); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// var routes = require('./api/routes/todoListRoutes'); //importing route
+// //routes(app); //register the route
+
+//load('models', {cwd: 'api' });
+load('controllers', {cwd: 'api'})
+  .then('routes')
+  .into(app);
+
+
+app.listen(port);
+
+app.use(function(req, res) { 
+    res.status(404).send({url: req.originalUrl + ' not found'})  
+});
+
+console.log('todo list RESTful API server started on: ' + port);
