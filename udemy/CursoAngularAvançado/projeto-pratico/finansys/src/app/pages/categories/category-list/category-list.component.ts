d0ea34f95d16bb3271d5bf1b73460e5707b0d8f9
Izan Calderaro <izancalderaro@yahoo.com.br>
2018-12-05 17:49:37 -0400

    msg de exclusao

--- udemy/CursoAngularAvançado/projeto-pratico/finansys/src/app/pages/categories/category-list/category-list.component.ts
+++ udemy/CursoAngularAvançado/projeto-pratico/finansys/src/app/pages/categories/category-list/category-list.component.ts
@@ -23,10 +23,14 @@ export class CategoryListComponent implements OnInit {
   }
 
   deleteCategory(category) {
+    const mustDelete = confirm ('Deseja realmente excluir este item');    
+
+    if (mustDelete) {
     this.categoryService.delete(category.id).subscribe(
       () => this.categories = this.categories.filter(element => element != category),
       () => alert('Erro ao tentar excluir')
     )
+   }
   }
 
 }
