eed95ba33e09d8e2e1338bd8f21fd7de08af5e71
Izan Calderaro <izancalderaro@yahoo.com.br>
2018-12-11 17:04:23 -0400

    inicio projeto

---
+++ udemy/ProjetoBaseTaskManagerAngular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Gerenciador de Tarefas';
+  task = 'Enviar orçamento para o cliente X'
+}
