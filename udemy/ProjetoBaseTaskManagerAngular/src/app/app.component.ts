8fe002f27d7f47d469f5bb793824fcbe2048d8df
Izan Calderaro <izancalderaro@yahoo.com.br>
2018-12-13 17:00:16 -0400

    adding o task component

--- udemy/ProjetoBaseTaskManagerAngular/src/app/app.component.ts
+++ udemy/ProjetoBaseTaskManagerAngular/src/app/app.component.ts
@@ -16,14 +16,13 @@ export class AppComponent {
 
 
 export class Task {
-
+ 
+  public id: number;
+  public title: string;
+  
   constructor(id: number, title: string) {
      this.id = id;
      this.title = title;    
   }
-
-  public id: number;
-  public title: string;
-
-
+  
 }
--- udemy/ProjetoBaseTaskManagerAngular/src/app/app.module.ts
+++ udemy/ProjetoBaseTaskManagerAngular/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { TasksComponent } from './tasks/tasks.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,9 +7,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+TasksComponent
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
