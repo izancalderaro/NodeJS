afa5946d5bc19b36fe8f1ed2f4b21b7e036531e7
Izan Calderaro <izancalderaro@yahoo.com.br>
2018-12-19 18:26:40 -0400

    aula 110

--- udemy/ProjetoBaseTaskManagerAngular/src/app/learning-bindings/learning-bindings.component.ts
+++ udemy/ProjetoBaseTaskManagerAngular/src/app/learning-bindings/learning-bindings.component.ts
@@ -3,10 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'learning-bindings',
     templateUrl: './learning-bindings.component.html'
-    
-})
-export class LearningBindingsComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit(): void { }
+})
+export class LearningBindingsComponent {
+
+    /**
+     * mouse events
+     * 
+     */
+    public onMouseOut() {
+        console.log('mouseout');
+
+    }
+
+    public onClick() {
+        console.log('click');
+
+    }
+
+    public onMouseOver() {
+        console.log('mouseover');
+
+    }
+
+    public onKeyDown (){
+        console.log('keydown');
+        
+    }
+
 }
