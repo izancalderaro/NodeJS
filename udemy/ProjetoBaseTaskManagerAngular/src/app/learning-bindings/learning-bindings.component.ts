15d1ef8d7d4f37d8af4e6326261351a09db3480c
Izan Calderaro <izancalderaro@yahoo.com.br>
2018-12-26 17:26:01 -0400

    aula 109

---
+++ udemy/ProjetoBaseTaskManagerAngular/src/app/learning-bindings/learning-bindings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+selector: 'larning-bindings',
+templateUrl:'./learning-bindings.component.html'
+})
+
+
+export class  LearningBindingsComponent{
+
+}
